afbf528cb05da8dfbdb0089297150ce5d4290f4d
Yashvi Agrawal <74981992+yashviagrawal@users.noreply.github.com>
2023-09-29 11:54:43 +0530

    Update: job count

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -35,7 +35,7 @@ class Dashboard extends StatelessWidget {
           // Job Statistics
           Container(
             padding: EdgeInsets.all(16.0),
-            color:  Color(0xFF186F65),
+            color: Color(0xFF186F65),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
@@ -45,18 +45,11 @@ class Dashboard extends StatelessWidget {
               ],
             ),
           ),
-// Ongoing Jobs (integrated widget)
+          // Ongoing Jobs (integrated widget)
           Expanded(
             child: OngoingJobsWidget(),
           ),
           // Padding added here
-          Padding(
-            padding: EdgeInsets.only(top: 16.0), // Adjust the top padding as needed
-            child: Text(
-              'Completed Jobs',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-          ),
           // Completed Jobs
           Expanded(
             child: ListView(
@@ -151,7 +144,7 @@ class _OngoingJobsWidgetState extends State<OngoingJobsWidget> {
     loadOngoingJobs();
   }
 
-// Function to load ongoing job data from form_data.json file
+  // Function to load ongoing job data from form_data.json file
   Future<void> loadOngoingJobs() async {
     try {
       final directory = await getExternalStorageDirectory(); // or getApplicationDocumentsDirectory()
